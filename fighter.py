56c7a99b5115b2533e1969a62eeaaa63be0f41ea
Sebastian Rojas <seba.nrc@gmail.com>
2020-10-01 19:25:23 -0300

    starting fighting game

---
+++ fighter.py
@@ -0,0 +1,14 @@
+class Fighter:
+    def __init__(self, name, hp, attack, defence):
+        self.name = self.name
+        self.hp = self.hp
+        self.attack_dmg = self.attack
+        self.defence = self.defence
+
+    def attack(self,b)
+        b.hp = (b.hp + b.defence) - self.attack_dmg
+
+    def show_status(self)
+        print(self.name, self.hp)
+
+
